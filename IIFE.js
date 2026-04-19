de8a09525f981b5985bff1124e3589655855fafb
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-04-19 12:15:51 +0530

    Day 4

---
+++ IIFE.js
@@ -0,0 +1,22 @@
+// Immediately Invoked Function Expression (IIFE)
+// IIFE is a function that is executed immediately after it is defined.
+
+// Named IIFE
+(function chai(){
+    console.log(`DB Connected`);
+})();
+
+//To avoid global scope pollution, we can use IIFE to create a new scope for our variables and functions, so that they do not interfere with the global scope and other functions and variables in the code. This is a good practice to avoid naming conflicts and to keep our code organized and modular.
+
+
+// Unnamed IIFE
+(()=>{console.log(`Function without a funcion name using IIFE`);
+})(); // Semi colon is added to the end to stop the context of the function from being mixed with the next line of code.
+
+
+// Unnamed IIFE with parameters
+((name)=>{
+    console.log(`Hello ${name} bhaisaab`);
+    
+})("Sirajuddin"); // The second () is used to pass the arguments to the function.
+
