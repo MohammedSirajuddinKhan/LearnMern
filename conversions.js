4c365755e592379c2e9d8c3be81064436ffd904a
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-04-20 13:32:25 +0530

    Day 2

---
+++ conversions.js
@@ -0,0 +1,11 @@
+// +++++++++++++++++ DAY 2 +++++++++++++++++
+
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof {score});
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(typeof NaN);
